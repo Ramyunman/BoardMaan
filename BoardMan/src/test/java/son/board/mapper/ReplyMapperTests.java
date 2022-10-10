57b725f609b3426a0340732fdb008dc86684083b
Ramyunman <prettyshield@naver.com>
2022-10-11 01:02:30 +0900

    22/10/11 part7 p.665

--- BoardMan/src/test/java/son/board/mapper/ReplyMapperTests.java
+++ BoardMan/src/test/java/son/board/mapper/ReplyMapperTests.java
@@ -19,28 +19,30 @@ import son.board.domain.ReplyVO;
 @Log4j
 public class ReplyMapperTests {
 	
-	// 테스트 전에 해당 번호의 게시물이 존재하는지 반드시 확인할 것
-	private Long[] bnoArr = { 3145745L, 3145744L, 3145743L, 3145742L, 3145741L };
 	
+	// 테스트 전에 해당 번호의 게시물이 존재하는지 반드시 확인할 것 
+	private Long[] bnoArr = { 3145745L, 3145744L, 3145743L, 3145742L, 3145741L };
+	 
 	@Setter(onMethod_ = @Autowired)
 	private ReplyMapper mapper;
 	
-	@Test
-	public void testCreate() {
-		
-		IntStream.rangeClosed(1, 10).forEach(i -> {
-			
-			ReplyVO vo = new ReplyVO();
-			
-			// 게시물의 번호
-			vo.setBno(bnoArr[i % 5]);
-			vo.setReply("댓글 테스트" + i);
-			vo.setReplyer("replyer" + i);
-			
-			mapper.insert(vo);
-		});
-	}
 	
+	@Test 
+	public void testCreate() {
+	  
+	  IntStream.rangeClosed(1, 10).forEach(i -> {
+	 
+	    ReplyVO vo = new ReplyVO();
+	 
+	    // 게시물의 번호 
+	    vo.setBno(bnoArr[i % 5]);
+	    vo.setReply("댓글 테스트" + i);
+	    vo.setReplyer("replyer" + i);
+	  
+	    mapper.insert(vo);
+	  });
+	}
+	 
 	@Test
 	public void testMapper() {
 		
@@ -65,28 +67,31 @@ public class ReplyMapperTests {
 		mapper.delete(targetRno);
 	}
 	
-	@Test
-	public void testUpdate() {
-		
-		Long targetRno = 10L;
-		
-		ReplyVO vo = mapper.read(targetRno);
-		
-		vo.setReply("Update Reply");
-		
-		int count = mapper.update(vo);
-		
-		log.info("UPDATE COUNT: " + count);
-	}
 	
-	@Test
-	public void testList() {
-		
-		Criteria cri = new Criteria();
-		//3145745L
-		List<ReplyVO> replies = mapper.getListWithPaging(cri, bnoArr[0]);
-		
-		replies.forEach(reply -> log.info(reply));
-	}
-
+	 @Test
+	 public void testUpdate() {
+	  
+	   Long targetRno = 10L;
+	  
+	   ReplyVO vo = mapper.read(targetRno);
+	 
+	   vo.setReply("Update Reply");
+	 
+	   int count = mapper.update(vo);
+	 
+	   log.info("UPDATE COUNT: " + count);
+	 }
+	 
+	
+	 @Test 
+	 public void testList() {
+	  
+	   Criteria cri = new Criteria();
+	   //3145745L 
+	   List<ReplyVO> replies = mapper.getListWithPaging(cri, bnoArr[0]);
+	  
+	   replies.forEach(reply -> log.info(reply));
+	   
+	 }
+	
 }
